fa5ce6a4e8c67d323efb4104e90ac1599d318a83
migueljsleca <migueljsleca@gmail.com>
2026-04-08 22:55:31 +0100

    Refresh homepage video asset

--- lib/marketing.ts
+++ lib/marketing.ts
@@ -1,1 +1,1 @@
-export const AUTH_HERO_VIDEO_URL = "https://9rq994vjgc9m0uyh.public.blob.vercel-storage.com/the-board-1.mp4";
+export const AUTH_HERO_VIDEO_URL = "https://9rq994vjgc9m0uyh.public.blob.vercel-storage.com/the-board-1.mp4?v=20260408-2";
